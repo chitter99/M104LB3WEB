0d0e34d3d84ef763d4787171636ea87b79170aa5
chitter99 <aaron.schmid@soxes.ch>
2017-03-30 13:21:27 +0200

    Updated config

--- resources/config.php
+++ resources/config.php
@@ -7,12 +7,19 @@ $config = array(
     'db' => array(
         'host' => 'localhost',
         'user' => 'root',
-        'passwd' => '',
+        'pass' => '',
         'database' => 'hotel'
     )
 );
 
 define('LIBRARY_PATH', realpath(dirname(__FILE__) . '/lib'));
 define('RESOURCES_PATH', realpath(dirname(__FILE__)));
+
+function GetDB()
+{
+    global $config;
+    require_once LIBRARY_PATH . '/hoteldb.class.php';
+    return new HotelDB($config['db']['host'], $config['db']['user'], $config['db']['passs'], $config['db']['database']);
+}
 
 ?>
