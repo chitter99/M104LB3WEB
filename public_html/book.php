762e2c1fc9f5045c9ab3901dda0d2cee8ec780ec
Marco Schneider <marco.schneider@somedia.ch>
2017-05-09 08:19:12 +0200

    Changed label in Form Changed styling

--- public_html/book.php
+++ public_html/book.php
@@ -57,44 +57,46 @@ $template_breadcrumbs = [
         <input type="hidden" name="to" value="<?php echo $search_from ?>" />
 
         <lable for="title">Anrede: </lable>
-        <select name="title">
+        <select class="book_form_select" name="title">
             <?php foreach($db->GetAllTitle() as $title): ?>
                 <option value="<?php echo $title['ID']; ?>"><?php echo $title['title']; ?></option>
             <?php endforeach; ?>
         </select>
-        <lable for="surname">Vorname:
-          <input type="text" name="surname" />
-        </lable>
-        <lable for="name">Nachname:
-          <input type="text" name="name" />
-        </lable>
+        <div>
+          <lable for="surname">Vorname:
+            <input class="book_form_name" type="text" name="surname" />
+          </lable>
+          <lable for="name">Nachname:
+            <input class="book_form_name" type="text" name="name" />
+          </lable>
+        </div>
         <lable for="mail">Mail:
-          <input type="text" name="mail" />
+          <input class="book_form_name" type="text" name="mail" />
         </lable>
         <lable for="phone">Telefonnummer:
-          <input type="text" name="phone" />
+          <input class="book_form_name" type="text" name="phone" />
         </lable>
         <lable for="addresse">Adresse:
-          <input type="text" name="address" />
+          <input class="book_form_name" type="text" name="address" />
         </lable>
         <lable for="birthday">Geburtstag:
-          <input type="date" name="birthday" />
+          <input class="book_form_name" type="date" name="birthday" />
         </lable>
         <lable for="city">Stadt:
-          <select name="city">
+          <select class="book_form_select" name="city">
               <?php foreach($db->GetAllCity() as $city): ?>
               <option value="<?php echo $city['ID']; ?>"><?php echo $city['plz'] . ' ' . $city['city']; ?></option>
               <?php endforeach; ?>
           </select>
         </lable>
         <lable for="room">Room:
-          <select name="room">
+          <select class="book_form_select" name="room">
               <?php foreach($db->SelectRoom(['fk_roomType' => $roomType['ID']])->fetch_all(MYSQLI_ASSOC) as $room): ?>
                   <option value="<?php echo $room['ID']; ?>"><?php echo $room['roomNumber']; ?></option>
               <?php endforeach; ?>
           </select>
         </lable>
-        <button type="submit">Buchen</button>
+        <button class="button" type="submit">Buchen</button>
     </form>
 </article>
 <?php require "./../template/footer.php"; ?>
